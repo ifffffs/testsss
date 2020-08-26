5b12577dd3a9b69a57ec78d5dba73974c81a25c4
shixin <1125069410@qq.com>
2020-08-26 22:33:07 +0800

    第一次python提交的代码

--- demo.py
+++ demo.py
@@ -1,6 +1,26 @@
-print{hello world}
+
+
+# print ("hello world ",end="你好好好好")
+# print (123456,end="你好好好好")
+# print (12.123,end="不好")
+# print (True,False)
+# print (())
+# print ([])
+# print ({})
+# print ("你好",666,"世界")
+# print ("haha"*10)
+
+# jjcc = 1+1+2*2%2
+
+# print (jjcc)
 
 
 
+# a = int(input ("输入"))
+# b = int(input ("请输入"))
 
-你好世界
+# print(a+b)
+
+
+a = (input("请输入： "))
+print (len(a)%2)
